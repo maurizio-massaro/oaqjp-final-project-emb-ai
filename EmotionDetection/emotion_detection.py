918942eb77115b80b8b950f13aab7fd9b81b8337
maurizio-massaro <maurizio.massaro@outlook.com>
2025-05-31 15:46:54 -0400

    Emotion detector package created

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    headers = {
+        "Content-Type": "application/json",
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+    data = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code == 200:
+        result = response.json()
+        emotion_scores = result['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+        emotion_scores["dominant_emotion"] = dominant_emotion
+        return emotion_scores
+    else:
+        return f"Request failed with status code {response.status_code}: {response.text}"
